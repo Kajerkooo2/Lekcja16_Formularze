f487059659c65696e248c94e570d9f445ddfec35
Kajerkooo2 <prokajerko79@gmail.com>
2026-04-21 08:18:43 +0200

    Logika(uwaga mam spinnera zamiast listView)

--- app/src/main/java/com/example/formularz_vet/MainActivity.java
+++ app/src/main/java/com/example/formularz_vet/MainActivity.java
@@ -1,6 +1,13 @@
 package com.example.formularz_vet;
 
 import android.os.Bundle;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.SeekBar;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +16,15 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText ownerNameInput;
+    private Spinner speciesSpinner;
+    private SeekBar ageSeekBar;
+    private TextView ageValue;
+    private EditText visitPurposeInput;
+    private EditText timeInput;
+    private Button okButton;
+    private TextView resultText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +36,55 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-    }
+        ownerNameInput = findViewById(R.id.ownerNameInput);
+        speciesSpinner = findViewById(R.id.speciesSpinner);
+        ageSeekBar = findViewById(R.id.ageSeekBar);
+        ageValue = findViewById(R.id.ageValue);
+        visitPurposeInput = findViewById(R.id.visitPurposeInput);
+        okButton = findViewById(R.id.okButton);
+        resultText = findViewById(R.id.resultText);
+        timeInput = findViewById(R.id.timeInput);
+
+        String[] species = {"Pies", "Kot", "Swinka Morska"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, species);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        speciesSpinner.setAdapter(adapter);
+
+        speciesSpinner.setOnItemSelectedListener(new android.widget.AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(android.widget.AdapterView<?> parent, android.view.View view, int position, long id) {
+                if (position == 0) ageSeekBar.setMax(18);
+                if (position == 1) ageSeekBar.setMax(20);
+                if (position == 2) ageSeekBar.setMax(9);
+
+                ageSeekBar.setProgress(0);
+                ageValue.setText("0");
+            }
+
+            @Override
+            public void onNothingSelected(android.widget.AdapterView<?> parent) {}
+        });
+
+        ageSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                ageValue.setText(String.valueOf(progress));
+            }
+
+            @Override public void onStartTrackingTouch(SeekBar seekBar) {}
+            @Override public void onStopTrackingTouch(SeekBar seekBar) {}
+        });
+
+        okButton.setOnClickListener(v -> {
+            String owner = ownerNameInput.getText().toString();
+            String speciesSelected = speciesSpinner.getSelectedItem().toString();
+            String age = ageValue.getText().toString();
+            String purpose = visitPurposeInput.getText().toString();
+            String time = timeInput.getText().toString();
+
+            String result = owner + ", " + speciesSelected + ", " + age + ", " + purpose + ", " + time;
+
+            resultText.setText(result);
+        });
+    };
 }
